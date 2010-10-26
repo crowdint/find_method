a4f6fe65b453327daa45c166f25ac306af01dc62
David Padilla <david@crowdint.com>
2010-10-26 12:42:26 -0500

    Even more DRYness (thanks @rafmagana)

--- lib/find_method.rb
+++ lib/find_method.rb
@@ -2,12 +2,8 @@ module FindMethod
   def find_method(query)
     methods.grep(Regexp.new(query))
   end
-
-  def self.included(receiver)
-    receiver.extend         FindMethod
-  end
 end
 
 class Object
-  include FindMethod
+  [:include, :extend].each {|m| send(m, FindMethod)}
 end
